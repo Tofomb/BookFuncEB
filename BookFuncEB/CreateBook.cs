9803e753dcbc4e71026a3da4b0dba5c86453f938
Tobias Forsberg <tobias.forsberg@solita.fi>
2022-10-31 10:44:13 +0100

    added createBook

--- BookFuncEB/CreateBook.cs
+++ BookFuncEB/CreateBook.cs
@@ -10,28 +10,28 @@ using Newtonsoft.Json;
 
 namespace BookFuncEB
 {
-    public static class Plunction
+    public static class CreateBook
     {
-        [FunctionName("Plunction")]
+        [FunctionName("CreateBook")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-            string lame = req.Query["lame"];
+            string title = req.Query["title"];
+            string author = req.Query["author"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-            lame = lame ?? data?.lame;
+            title = title ?? data?.title;
+            author = author ?? data?.author;
 
-            string responseMessage = "hmmm...";
+            string responseMessage = "hmmm... add title and author to the body in order to create a new book";
 
-            if (!string.IsNullOrEmpty(lame) && !string.IsNullOrEmpty(name))
+            if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(author))
             {
-                responseMessage = $"{lame} {name} or something";
+                responseMessage = $"{title} is written by {author}";
             }
             return new OkObjectResult(responseMessage);
         }
